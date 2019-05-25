d97123563a1a524db8636c00b82bb6571b774c42
543877815 <543877815@qq.com>
2019-05-25 18:04:43 +0800

    fix merge and add create new .exe and add README.md

---
+++ os/README.md
@@ -0,0 +1,9 @@
+用于生成新的可执行文件
+
+```bash
+pyinstaller -F main.py
+```
+
+传入文件名的参数则自动按列读取test shell作为输入
+
+否则等待用户输入
--- os/main.py
+++ os/main.py
@@ -138,7 +138,6 @@ def analysis(inputs):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     processor, resource = system_init()
     if len(sys.argv) == 2:
         filename = sys.argv[1]
@@ -149,12 +148,3 @@ if __name__ == '__main__':
             code = analysis(X)
             if code == -1:
                 break
-=======
-    print("Process init is running")
-    processor, resource = systeminit()
-    while (True):
-        X = input()
-        code = analysis(X)
-        if code == -1:
-            break
->>>>>>> master
